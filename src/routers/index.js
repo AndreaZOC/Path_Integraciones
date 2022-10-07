13d99c69a3a4a6f0518fc74814a451e785e26163
AndreaO <andrea.ortiz@coreapps.mx>
2022-10-07 18:28:12 -0500

    post

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,25 @@ const { Router } = require('express');
 const router = Router();
 //const _ = require('underscore');
 
-const datos = require('../bd/datos.json');
+const integraciones = require('../bd/datos.json');
 
 router.get('/', (req, res) => {
-    res.json(datos);
+    res.json(integraciones);
 });
+
+router.post('/', (req, res) => {
+    const { cliente, version, area, path, user, password, autor } = req.body;
+    if (cliente && version && area && user && password && path && autor) {
+        const id = integraciones.length + 1;
+        const test = "jiji"; //agrega un campo con info que no esta en la BD
+        const newIntegracion = { id, ...req.body, test}
+        integraciones.push(newIntegracion);
+        res.json(integraciones);
+    } else {
+        res.send('Wrong Request');
+    }
+});
+
+
 
 module.exports = router;
