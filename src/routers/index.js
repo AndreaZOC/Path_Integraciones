7919dc3f0864f5cb1170866d8396b7a0de3bb890
AndreaO <andrea.ortiz@coreapps.mx>
2022-10-07 19:04:14 -0500

    put

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-//const _ = require('underscore');
+const _ = require('underscore');
 
 const integraciones = require('../bd/datos.json');
 
@@ -12,13 +12,37 @@ router.post('/', (req, res) => {
     const { cliente, version, area, path, user, password, autor } = req.body;
     if (cliente && version && area && user && password && path && autor) {
         const id = integraciones.length + 1;
-        const newIntegracion = { id, ...req.body}
+        const newIntegracion = { id, ...req.body }
         integraciones.push(newIntegracion);
         res.json(integraciones);
     } else {
         res.send('Wrong Request');
     }
 });
+
+//revisar para mas parametros
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { cliente, version, area, path, user, password, autor } = req.body;
+    if (cliente && version && area && user && password && path && autor) {
+        _.each(integraciones, (integracion, i) => {
+            if (integracion.id == id) {
+                integracion.cliente = cliente;
+                integracion.version = version;
+                integracion.area = area;
+                integracion.path = path;
+                integracion.user = user;
+                integracion.password = password;
+                integracion.autor = autor;
+            }
+        });
+        res.json(integraciones)
+    } else {
+        res.status(500).json({ error: 'Error' });
+    }
+
+});
+
 
 
 
